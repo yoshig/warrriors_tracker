e5be3bc310116ae33a91254bdfac22a850a839a5
Will Hastings <will@objectdotcreate.net>
2014-02-12 21:38:43 -0800

    Added player and shot models

---
+++ warriors/db/migrate/20140213051534_create_shots.rb
@@ -0,0 +1,10 @@
+class Shots < ActiveRecord::Migration
+  def change
+    create_table :shots do |t|
+      t.integer :player_id
+      t.integer :points
+      t.boolean :made
+      t.string  :shot_type
+    end
+  end
+end
